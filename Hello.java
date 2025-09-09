d524084f9a0c5383c1805df1c318dd75f64261cf
Sarfraz-g19 <mohammadsarfraj2001@gmail.com>
2025-09-09 11:10:24 +0530

    Update and rename Hello.java to Hello.py

--- Hello.java
+++
@@ -1,8 +0,0 @@
-public class Hello {
-    public static void main(String[] args) {
-        // Run a loop 100 times
-        for (int i = 1; i <= 100; i++) {
-            System.out.println("Run number: " + i + " - nitish land jhantu!");
-        }
-    }
-}
---
+++ Hello.py
@@ -0,0 +1,4 @@
+# Hello.py
+
+for i in range(1, 101):  # Loop from 1 to 100
+    print(f"Run number: {i} - nitish jhantu!")
